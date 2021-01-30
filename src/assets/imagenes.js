6f0656a4b9c18b3a408b027258cc2d76c4942841
jalilmanuel25 <jalilmanuel25@gmail.com>
2021-01-30 08:58:04 -0600

    Maquetación seccs inicio, conoce producs, nosotros

--- src/assets/imagenes.js
+++ src/assets/imagenes.js
@@ -1,9 +1,15 @@
 import logo from './images/logo.png';
 import iot from './images/iot.jpg';
 import smarthome from './images/smarthome.png';
+import background from './images/background.png';
+import workflowitems from './images/workflow-items.png';
+import workflowslide from './images/workflow-slide.png';
 
 export default {
     "logo": logo,
     "iot": iot,
-    "smarthome": smarthome
+    "smarthome": smarthome,
+    "background": background,
+    "workflowitems": workflowitems,
+    "workflowslide": workflowslide
 }
--- src/components/Home.js
+++ src/components/Home.js
@@ -21,6 +21,34 @@ class Home extends Component {
                     <img src={this.props.imagenes.smarthome}></img>
                 </div>
             </div>
+            <section className="nosotros">
+                <img className="workflow-items" src={this.props.imagenes.workflowitems}/>
+                <div className="contenedor contenedor-nosotros">
+                    <h2 className="titulo">Nosotros</h2>
+                    <p>
+                    Somos una empresa 100% duranguense que tiene el principal
+                    objetivo de desarrollar y vender aplicaciones de software y prototipos de internet de las cosas de alta calidad, cubriendo las necesidades de  sus clientes y mejorando el funcionamiento de algunas cosas de su vida cotidiana.
+                    </p>
+                </div>
+                <img className="workflow-slide" src={this.props.imagenes.workflowslide}/>
+            </section>
+            <section className="contacto">
+                <div className="contenedor contendor-contacto">
+                    <h2 className="titulo texto-azul">Contacto</h2>
+                    <div className="info-formulario">
+                        <div className="info">
+                            <h3>Información de contacto</h3>
+                            <p>¿Tienes alguna duda? Solo escribenos en nuestras
+                            redes o en nuestro formulario.</p>
+                            <p>6181846889</p>
+                            <p>brightit@gmail.com</p>
+                        </div>
+                        <form>
+                            <label>Envianos un mensaje</label>
+                        </form>
+                    </div>
+                </div>
+            </section>
         </div>
     }
 }
