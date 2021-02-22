543fd65f8837243f2e4beaf51f956b4d243bd698
jalilmanuel25 <jalilmanuel25@gmail.com>
2021-02-22 13:52:57 -0600

    Se agrega método de consultar usuarios

--- src/controllers/users.controller.js
+++ src/controllers/users.controller.js
@@ -3,9 +3,30 @@ const usersCtrl = {};
 const User = require('../models/User');
 
 usersCtrl.verUsuarios = (req, res) => {
-    return res.status(200).send({
-        // Registros consultados con éxito
-        status: 'Busqueda Correcta'
+    var consulta = User.find({});
+
+    consulta.exec((err, usuarios) => {
+        if(err) {
+            return res.status(500).send({
+                // Error
+                status: 'Error',
+                mensaje: 'Error al devolver usuarios'
+            });
+        }
+
+        if(!usuarios) {
+            return res.status(404).send({
+                // Error
+                status: 'Error',
+                mensaje: 'No existen usuarios en la colección'
+            });
+        }
+
+        return res.status(200).send({
+            // Registros consultados con éxito
+            status: 'Busqueda Correcta',
+            usuarios
+        });
     });
 }
 
@@ -13,7 +34,22 @@ usersCtrl.agregarUsuario = async (req, res) => {
     const {name, email, password} = req.body;
 
     const newUser = new User({name, email, password});
-    await newUser.save();
+
+    await newUser.save((err, usuarioAgregado) => {
+
+        if (err || !usuarioAgregado) {
+            return res.status(404).send({
+                status: 'error',
+                mensaje: 'El usuario no se ha guardado'
+            })
+        }
+
+        return res.status(200).send({
+            //Registro insertado con exito 
+            status: 'Completado',
+            usuarioAgregado
+        });
+    });
 }
 
 module.exports = usersCtrl;
