666c3416bcac9af8f5bb61c712601501307485ef
jalilmanuel25 <jalilmanuel25@gmail.com>
2021-02-26 13:41:27 -0600

    Se agregan componentes de React

--- src/app/App.js
+++ src/app/App.js
@@ -1,11 +1,58 @@
-import React from 'react';
+import React, {Component} from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css'
+import {BrowserRouter as Router, Route, Link} from 'react-router-dom';
+import Navbar from './components/Navbar/Navbar';
+import Home from './components/Home/Home.js';
+// import Login from './components/Login.js';
+// import CrearCuenta from './components/CrearCuenta.js';
+// import imagenes from './assets/imagenes';
+import Productos from './components/Productos/Productos.js';
+// import Producto from './components/Producto.js';
+// import Dashboard from './components/Dashboard.js';
 
-function App() {
-  return (
-    <div className='app'>
-      <h1>Hola Mundo!</h1>
+class App extends Component {
+  render() {
+    return <div>
+      <Router>
+        <Navbar/>
+        <Route exact path="/" render={() => {
+            return <div>
+            	<Home/>
+            </div>
+          }}>
+        </Route>
+        <Route exact path="/login" render={() => {
+            return <div>
+              {/* <Login imagenes={imagenes}/> */}
+            </div>
+          }}>
+        </Route>
+        <Route exact path="/crear_cuenta" render={() => {
+            return <div>
+              {/* <CrearCuenta imagenes={imagenes}/> */}
+            </div>
+          }}>
+        </Route>
+        <Route exact path="/productos" render={() => {
+            return <div>
+              <Productos/>
+            </div>
+          }}>
+        </Route>
+        <Route exact path="/producto" render={() => {
+            return <div>
+              {/* <Producto imagenes={imagenes}/> */}
+            </div>
+          }}>
+        </Route>
+        <Route exact path="/dashboard" render={() => {
+            return <div>
+              {/* <Dashboard imagenes={imagenes}/> */}
+            </div>
+          }}>
+        </Route>
+      </Router>
     </div>
-  );
+  }
 }
-
 export default App;
